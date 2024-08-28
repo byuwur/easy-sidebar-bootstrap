e89c3b68fe06a03b4e272d06e9dafceda0757ad1
Mateus <atrujillomateus@gmail.com>
2024-08-27 23:35:15 -0500

    Cookies func. Remove unused func. Centralize _common func.

--- _common.js
+++ _common.js
@@ -1,68 +1,89 @@
 "use strict";
 /*
- * File: common.js
+ * File: _common.js
  * Desc: Contains common resources that are initialized in a per-page basis instead of globally.
  * Deps: jQuery, /_functions.js
  * Copyright (c) 2023 Andrés Trujillo [Mateus] byUwUr
  */
 
-// Check if jQuery is loaded before running the script
-if (window.jQuery)
-	$(() => {
-		console.log("Init common.js");
-		// Expand the sidebar automatically on larger screens (min-width: 768px)
-		if (window.matchMedia("(min-width: 768px)").matches) {
-			$("#sidebar-toggle").addClass("sidebar-expanded");
-			$("#sidebar").addClass("sidebar-expanded");
-			$(".app-container").addClass("sidebar-expanded");
-		}
-		// Toggle sidebar expansion when the sidebar toggle button is clicked
-		$("#sidebar-toggle")
-			.off("click")
-			.on("click", function () {
-				$("#sidebar-toggle").trigger("blur");
-				$("#sidebar .overlay").css("height", "");
-				if (!$("#sidebar-toggle").hasClass("sidebar-expanded")) {
-					$("#sidebar-toggle").addClass("sidebar-expanded");
-					$("#sidebar").addClass("sidebar-expanded");
-					$(".app-container").addClass("sidebar-expanded");
-					$("#sidebar-hidden").css("display", "none");
-				} else {
-					$("#sidebar-toggle").removeClass("sidebar-expanded");
-					$("#sidebar").removeClass("sidebar-expanded");
-					$(".app-container").removeClass("sidebar-expanded");
-					$("#sidebar-hidden").css("display", "flex");
-					$("#sidebar").scrollTop(0);
-				}
-			});
-		// Expand sidebar when the hidden sidebar area is hovered
-		$("#sidebar-hidden")
-			.off("mouseenter")
-			.on("mouseenter", function () {
-				if (!$("#sidebar-toggle").hasClass("sidebar-expanded")) $("#sidebar").addClass("sidebar-expanded");
-			});
-		// Collapse sidebar when the mouse leaves the hidden sidebar area
-		$("#sidebar-hidden")
-			.off("mouseleave")
-			.on("mouseleave", function () {
-				if (!$("#sidebar-toggle").hasClass("sidebar-expanded") && !$("#sidebar").is(":hover")) $("#sidebar").removeClass("sidebar-expanded");
-			});
-		// Ensure the overlay inside the sidebar follows it accordingly, due to being an absolute positioned inside another
-		let sidebarScrollTop = 0;
-		$("#sidebar")
-			.off("scroll")
-			.on(
-				"scroll",
-				debounce(function () {
+/**
+ * Initializes the <Sidebar /> component in #spa-nav.
+ */
+function initSidebar() {
+	// Check it exists in the first place. Duh..
+	if (!$("#sidebar").length) return;
+	console.log("Init <Sidebar />");
+	if (!getCookie("SidebarExpand")) setCookie("SidebarExpand", "on");
+	// Ensure the overlay inside the sidebar follows it accordingly, due to being an absolute positioned inside another
+	$("#sidebar")
+		.off("scroll")
+		.on(
+			"scroll",
+			(function () {
+				let sidebarScrollTop = 0;
+				return debounce(function () {
 					const top = Math.floor($(this).scrollTop()),
 						diff = top - sidebarScrollTop;
+					console.log(top);
 					if ($("#sidebar").hasClass("sidebar-expanded")) $("#sidebar .overlay").css("height", `${$("#sidebar .overlay").height() + diff}px`);
 					sidebarScrollTop = top;
-				})
-			)
-			// Ensure the sidebar collapses when the mouse leaves the sidebar itself
-			.off("mouseleave")
-			.on("mouseleave", function () {
-				if (!$("#sidebar-toggle").hasClass("sidebar-expanded")) $("#sidebar").removeClass("sidebar-expanded");
-			});
-	});
+				});
+			})()
+		)
+		// Ensure the sidebar collapses when the mouse leaves the sidebar itself
+		.off("mouseleave")
+		.on("mouseleave", function () {
+			if (!$("#sidebar-toggle").hasClass("sidebar-expanded")) $("#sidebar").removeClass("sidebar-expanded");
+		});
+	// Toggle sidebar expansion when the sidebar toggle button is clicked
+	$("#sidebar-toggle")
+		.off("click")
+		.on("click", function () {
+			$("#sidebar-toggle").trigger("blur");
+			$("#sidebar .overlay").css("height", "");
+			if (!$("#sidebar-toggle").hasClass("sidebar-expanded")) {
+				$("#sidebar-toggle").addClass("sidebar-expanded");
+				$("#sidebar").addClass("sidebar-expanded");
+				$(".app-container").addClass("sidebar-expanded");
+				$("#sidebar-hidden").css("display", "none");
+				setCookie("SidebarExpand", "on");
+			} else {
+				$("#sidebar-toggle").removeClass("sidebar-expanded");
+				$("#sidebar").removeClass("sidebar-expanded");
+				$(".app-container").removeClass("sidebar-expanded");
+				$("#sidebar-hidden").css("display", "flex");
+				$("#sidebar").scrollTop(0);
+				setCookie("SidebarExpand", "off");
+			}
+		});
+	// Expand sidebar when the hidden sidebar area is hovered
+	$("#sidebar-hidden")
+		.off("mouseenter")
+		.on("mouseenter", function () {
+			if (!$("#sidebar-toggle").hasClass("sidebar-expanded")) $("#sidebar").addClass("sidebar-expanded");
+		});
+	// Collapse sidebar when the mouse leaves the hidden sidebar area
+	$("#sidebar-hidden")
+		.off("mouseleave")
+		.on("mouseleave", function () {
+			if (!$("#sidebar-toggle").hasClass("sidebar-expanded") && !$("#sidebar").is(":hover")) $("#sidebar").removeClass("sidebar-expanded");
+		});
+	// Expand the sidebar automatically on larger screens (min-width: 768px)
+	if (window.matchMedia("(min-width: 768px)").matches && getCookie("SidebarExpand") == "on") {
+		$("#sidebar-toggle").addClass("sidebar-expanded");
+		$("#sidebar").addClass("sidebar-expanded");
+		$(".app-container").addClass("sidebar-expanded");
+	}
+}
+
+/**
+ * Initializes all components that dynamically changes within the page
+ */
+function initCommon() {
+	if (!window.jQuery) {
+		console.error("Init common.js FAILED. No jQuery found.");
+		return;
+	}
+	console.log("Init common.js");
+	initSidebar();
+}
--- _functions.js
+++ _functions.js
@@ -7,9 +7,26 @@
  */
 
 /**
+ * Creates or updates a cookie with the specified name, value, and expiration days.
+ * @param {string} name The name of the cookie.
+ * @param {string} value The value of the cookie.
+ * @param {number} minutes (Default 1y) The number of days until the cookie expires. A negative number expires the cookie.
+ */
+function setCookie(name, value, minutes = 31536000) {
+	document.cookie = `${name}=${encodeURIComponent(value)};max-age=${minutes};path=/`;
+}
+
+/**
+ * Retrieves the value of the cookie with the specified name.
+ * @param {string} name The name of the cookie to retrieve.
+ * @return {string | null} The value of the cookie or null if not found.
+ */
+function getCookie(name) {
+	return `; ${document.cookie}`.split(`; ${name}=`).pop().split(";").shift() || null;
+}
+
+/**
  * Creates a debounced function that delays the execution of the provided function (`func`)
- * until after the specified `wait` time has elapsed since the last time the debounced function was called.
- *
  * @param {Function} func The function to debounce. This is the function that will be delayed in execution.
  * @param {number} wait (Default 111) The number of milliseconds to wait before executing the `func`.
  * @return {Function} Returns a new debounced version of the `func` that delays its execution.
@@ -20,45 +37,4 @@ function debounce(func, wait = 250) {
 		clearTimeout(timeout);
 		timeout = setTimeout(() => func.apply(this, arguments), wait);
 	};
-}
-
-/**
- * Hides and removes the front modal from the DOM after a short delay.
- * @param {boolean} $delay (Default True) Whether delays its detruction or not.
- */
-function destroy_modal_front($delay = true) {
-	$("#modal_front").modal("hide");
-	setTimeout(() => $("#modal_front").remove(), $delay ? 111 : 1);
-}
-
-/**
- * Displays a Bootstrap modal with a customizable message and actions.
- * @param {string} $state The state of the modal (e.g., success, danger, info, warning).
- * @param {string} $title The title of the modal.
- * @param {string} $message The message to display in the modal.
- * @param {boolean} $hideCancelBtn Whether to hide the cancel button.
- * @param {string} $redirect The URL to redirect to when "OK" is clicked.
- */
-function show_modal_front($state = "success", $title = "INFO.", $message = "Message.", $hideCancelBtn = false, $redirect = "javascript:destroy_modal_front();") {
-	// If already exist, destroy it with no delay
-	if ($("#modal_front").length) destroy_modal_front(false);
-	// HTML structure for the modal
-	let modal_front = `<div id="modal_front" class="modal fade" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel">
-        <div id="modal_front_container" class="modal-dialog modal-dialog-scrollable">
-            <div class="modal-content">
-                <div id="modal_front_title" class="modal-header m-0 fs-5 alert alert-${$state}">${$title}</div>
-                <div id="modal_front_body" class="modal-body text-dark">${$message}</div>
-                <div class="modal-footer">
-                    <a id="modal_front_back" class="btn btn-dark" href="javascript:destroy_modal_front();" onclick="javascript:destroy_modal_front();">CANCEL</a>
-                    <a id="modal_front_ok" class="btn btn-success" href="${$redirect}">OK</a>
-                </div>
-            </div>
-        </div>
-    </div>`;
-	// Append it to the <body>
-	$("body").append(modal_front);
-	$hideCancelBtn ? $("#modal_front_back").addClass("d-none") : $("#modal_front_back").removeClass("d-none");
-	// Center if window size merits and show
-	window.innerWidth < 992 ? $("#modal_front_container").addClass("modal-dialog-centered") : $("#modal_front_container").removeClass("modal-dialog-centered");
-	$("#modal_front").modal("show");
 }
